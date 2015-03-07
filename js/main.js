0606ab016a205434bde73e8978b871f6d1518c27
Miha Vidakovic <miha@vidakovic.si>
2015-03-07 01:08:29 +0100

    gh-pages

--- js/main.js
+++
@@ -1,8 +0,0 @@
-$(window).scroll(function(e) {
-  var scrollTop = window.scrollY;
-  if (scrollTop > 300) {
-    $('.about img:not(.visible)').addClass('visible');
-  } else {
-    $('.about img.visible').removeClass('visible');
-  }
-});
